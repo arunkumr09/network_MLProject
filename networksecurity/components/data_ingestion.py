5395b820ab4408ba2debf0a8523c33ad2b123818
arunkumr09 <arunkumr6006@gmail.com>
2025-03-28 20:53:21 +0530

    data validation

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -6,7 +6,6 @@ from networksecurity.logging.logger import logging
 
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
-
 import os
 import sys
 import numpy as np
@@ -94,11 +93,9 @@ class DataIngestion:
             dataframe=self.export_collection_as_dataframe()
             dataframe=self.export_data_into_feature_store(dataframe)
             self.split_data_as_train_test(dataframe)
-            dataingestionartifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
-                                                          test_file_path=self.data_ingestion_config.testing_file_path)
-            
+            dataingestionartifact=DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
+                                                        test_file_path=self.data_ingestion_config.testing_file_path)
             return dataingestionartifact
-            
 
         except Exception as e:
             raise NetworkSecurityException
